c8a49e7ca620226937555a5d34b3f97c9e3b1aca
Gray Pham <hunghtk456@gmail.com>
2023-07-02 22:14:22 +0700

    chore: u

--- src/Modules/Modules.Pleiger.CommonModels/Models/KIO_ClientSoundMgt.cs
+++ src/Modules/Modules.Pleiger.CommonModels/Models/KIO_ClientSoundMgt.cs
@@ -22,6 +22,7 @@ namespace Modules.Pleiger.CommonModels.Models
         public DateTime? lastModified { get; set; }
         public string soundType { get; set; }
         public string typeName { get; set; }
+        public string Version { get; set; }
     }
     public class ResultAudioDto
     {
